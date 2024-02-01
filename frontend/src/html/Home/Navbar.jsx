959e42bcc303c09ccc3198f73e9b70c683b592a1
Shruti-Bansal-Gupta <shrutidev.bansal@gmail.com>
2024-02-01 11:20:51 +0530

    updated the front page

--- frontend/src/html/Home/Navbar.jsx
+++ frontend/src/html/Home/Navbar.jsx
@@ -1,23 +1,201 @@
 import React from 'react';
-import '../../css/Navbar.css';
+import '../../../src/css/all.css'
+import bulb from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/bulb-1@2x.png'
+import rec3 from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/rectangle-3.svg'
+import comm1 from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/community-1.svg'
+import curious from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/curiosity-brainamico-1.svg'
+import down from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/download-1@2x.png'
+import event from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/events-1@2x.png'
+import frame from '/Users/shrut/OneDrive/Desktop/LikeMinds/like_minds/frontend/src/public/frame-1.svg'
 
 function Navbar() {
   return (
-    <div >
+    <div className='maindiv'>
+    <div className="desktop-1">
+      <section className="frame-root">
+        <header className="frame-home-communities-resourc">
+          <div className="frame-home-community-resources">
+            <img
+              className="bulb-1-icon"
+              loading="eager"
+              alt=""
+              src={bulb}
+            />
 
-       <div id='nav-bar'> 
-       <img src="brain.png" alt="logo" width="100%" height="100%" />
-       <ul>
-  <li><a href="">Home</a></li>
-  <li><a href="">Communities</a></li>
-  <li><a href="">Events</a></li>
-  <li><a href="">Resources</a></li>
+            <b className="likeminds">LikeMinds</b>
+          </div>
+          <div className="frame-home">
+            <img
+              className="frame-home-child"
+              alt=""
+              src={rec3}
+            />
 
-</ul>
-   <button id='button'>Login</button>
-   <button id='button'>Sign Up</button>
-       </div>
+            <div className="frame-communities">
+              <div className="home">Home</div>
+            </div>
+            <div className="events">Events</div>
+            <div className="frame-communities1">
+              <div className="communities">Communities</div>
+            </div>
+            <div className="frame-communities2">
+              <div className="resources">Resources</div>
+            </div>
+            <div className="frame-login">
+              <button className="frame-sign-up">
+                <div className="frame-sign-up-child"></div>
+                <div className="login">Login</div>
+              </button>
+            </div>
+            <button className="frame-connect">
+              <div className="frame-connect-child"></div>
+              <div className="frame-curiosity-brainamico">
+                <div className="sign-up">Sign Up</div>
+              </div>
+            </button>
+          </div>
+        </header>
+        <div className="text-question">
+          <i className="are-you-in-container">
+            <p className="are-you-in">Are you in search of some like</p>
+            <p className="minds-to-be">minds to be with, to learn with,</p>
+            <p className="to-grow-with">to grow with?</p>
+          </i>
+        </div>
+        <div className="text-placeholder">
+          <div className="text-placeholder-child"></div>
+          <img
+            className="frame-empty-icon"
+            loading="eager"
+            alt=""
+            src={frame}
+          />
+        </div>
+      </section>
+      <div className="so-what-are-container">
+        <p className="so-what-are-you-waiting-for">
+          <span>
+            <span>
+              <span className="so-what-are">So, what are you </span>
+              <span className="waiting">waiting</span>
+              <span> for?</span>
+            </span>
+          </span>
+        </p>
+        <p className="lets-connect-on-likem">
+          <span>
+            <span>
+              <span>Let’s Connect on </span>
+            </span>
+            <span>
+              <span className="span"> </span>
+            </span>
+          </span>
+          <span>
+            <span className="likeminds1">
+              <span className="likeminds2">LikeMinds</span>
+            </span>
+          </span>
+        </p>
+      </div>
+      <section className="wait-frame">
+        <div className="curiosity-frame">
+          <div className="curiosity-brainamico-frame">
+            <div className="curiosity-brainamico-frame-child"></div>
+            <img
+              className="curiosity-brain-amico-1"
+              loading="eager"
+              alt=""
+              src={curious}
+            />
+
+            <div className="download-frame">
+              <div className="question-frame">
+                <div className="got-a-question-container">
+                  <p className="got-a-question">
+                    <span>
+                      <span className="got-a">Got a </span>
+                      <span className="question">question</span>
+                    </span>
+                    <span className="span1">
+                      <span className="span2"> </span>
+                    </span>
+                    <span>
+                      <span className="span3">?</span>
+                    </span>
+                  </p>
+                  <p className="ask-here">
+                    <span>
+                      <span>Ask here.</span>
+                    </span>
+                  </p>
+                </div>
+                <div className="frame-frame">
+                  <textarea className="like-minds-text" rows={5} cols={13}></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="curiosity-brainamico-frame1">
+            <div className="curiosity-brainamico-frame-item"></div>
+            <div className="want-to-be-part-of-a-community-parent">
+              <div className="want-to-be-container">
+                <p className="want-to-be">Want to be part</p>
+                <p className="of-a">of a</p>
+                <p className="community">
+                  <b className="b"> </b>
+                  <span className="community1">
+                    <span>Community</span>
+                    <span className="span4"> </span>
+                  </span>
+                  <span className="span5">
+                    <span className="span6">?</span>
+                  </span>
+                </p>
+              </div>
+              <div className="events-frame">
+                <textarea className="community-frame" rows={5} cols={13}></textarea>
+              </div>
+            </div>
+            <img
+              className="download-1-icon"
+              loading="eager"
+              alt=""
+              src={down}
+            />
+          </div>
+          <div className="curiosity-brainamico-frame2">
+            <div className="curiosity-brainamico-frame-inner"></div>
+            <img
+              className="events-1-icon"
+              loading="eager"
+              alt=""
+              src={event}
+            />
+          </div>
+        </div>
+      </section>
+      <section className="rectangle-frame">
+        <div className="participate-frame"></div>
+      </section>
+      <section className="community-frame1">
+        <div className="participate-in-the-container">
+          <p className="participate-in-the-upcoming">
+            <span>
+              <span>Participate in the upcoming</span>
+              <span className="span7"> </span>
+            </span>
+          </p>
+          <p className="events1">Events</p>
+        </div>
+        <div className="events-text">
+          <textarea className="rectangle-frame1" rows={5} cols={13}></textarea>
+        </div>
+        <img className="community-1-icon" alt="" src={comm1} />
+      </section>
     </div>
-  )
+    </div>
+  );
 }
+
 export default Navbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Navbar from './html/Home/Navbar.jsx';
 import Footer from './html/Home/Footer.jsx';
 import App from './App.jsx'
+import Text from './html/Home/Text.jsx'
+import Login from './html/Login/loginpage.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +15,9 @@ root.render(
     <Navbar/>
     <Routes>
      <Route path = "/app" element={<App />} />
+     <Route path ="/text" element={<Text/>} />
+     <Route path ="/login" element={<Login/>} />
     </Routes>
-    <Footer/>
   </BrowserRouter>
 );
 
